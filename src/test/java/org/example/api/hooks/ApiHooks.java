fffa0a64f6662ff39848f3209648909418d61516
AboubakarAhamada <aboumarshal@hotmail.fr>
2026-02-22 18:20:50 +0100

    Updating the Hook class

--- src/test/java/org/example/api/hooks/ApiHooks.java
+++ src/test/java/org/example/api/hooks/ApiHooks.java
@@ -3,8 +3,18 @@ package org.example.api.hooks;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import org.example.api.support.SharedContext;
+
+import java.nio.charset.StandardCharsets;
 
 public class ApiHooks {
+    private final SharedContext sharedContextcontext;
+
+    public ApiHooks(SharedContext sharedContextcontext) {
+        //Cucumber injects the same context into all steps and hooks
+        this.sharedContextcontext = sharedContextcontext;
+    }
+
     @Before
     public void messageBeforeScenario(Scenario scenario){
         System.out.println("=======================================================================================\n" +
@@ -14,6 +24,13 @@ public class ApiHooks {
 
     @After
     public void messageAfterScenario(Scenario scenario){
+        if(scenario.isFailed() && sharedContextcontext.lastResponse != null){
+            scenario.attach(
+                    sharedContextcontext.lastResponse.asPrettyString().getBytes(),
+                    "application/json",
+                    "response-body"
+            );
+        }
         System.out.println("=======================================================================================\n" +
                 "                       Fin du scenario <<" +scenario.getName()+">>\n"+
                 "=======================================================================================");
