6ebee6fffbeb9509a861046b893ec54bfc2fc50f
AboubakarAhamada <aboumarshal@hotmail.fr>
2026-03-01 21:26:13 +0100

    Adding SharedContext class in order to share object data within the same scenario's steps

--- src/test/java/org/example/api/support/SharedContext.java
+++ src/test/java/org/example/api/support/SharedContext.java
@@ -1,7 +1,10 @@
 package org.example.api.support;
 
 import io.restassured.response.Response;
+import lombok.Data;
 
+@Data
 public class SharedContext {
-    public Response lastResponse;
+    private Response lastResponse;
+    private Integer postId;
 }
