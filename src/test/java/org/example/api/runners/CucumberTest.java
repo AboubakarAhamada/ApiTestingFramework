80b09d1a4d9ffbea65526a62e93d690cadaf5efe
AboubakarAhamada <aboumarshal@hotmail.fr>
2026-03-16 17:16:53 +0100

    Renamming class

--- src/test/java/org/example/api/runners/CucumberTest.java
+++ src/test/java/org/example/api/runners/CucumberTest.java
@@ -12,5 +12,5 @@ import org.junit.platform.suite.api.Suite;
 @ConfigurationParameter(key = "cucumber.glue", value = "org.example.api")
 @ConfigurationParameter(key = "cucumber.plugin", value = "pretty, summary, html:target/cucumber-report.html, json:target/cucumber.json")
 @ConfigurationParameter(key = "cucumber.filter.tags", value = "not @ignore")
-public class CucumberTestSuite {
+public class CucumberTest {
 }
