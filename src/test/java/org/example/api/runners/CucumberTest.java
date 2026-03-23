dd34e3851213e5246dc8545d047fff8f5417c02f
AboubakarAhamada <aboumarshal@hotmail.fr>
2026-03-23 17:04:54 +0100

    code review

--- src/test/java/org/example/api/runners/CucumberTest.java
+++ src/test/java/org/example/api/runners/CucumberTest.java
@@ -1,14 +1,11 @@
 package org.example.api.runners;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.*;
 
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
+@SelectPackages("features")
 @ConfigurationParameter(key = "cucumber.glue", value = "org.example.api")
 @ConfigurationParameter(key = "cucumber.plugin", value = "pretty, summary, html:target/cucumber-report.html, json:target/cucumber.json")
 @ConfigurationParameter(key = "cucumber.filter.tags", value = "not @ignore")
